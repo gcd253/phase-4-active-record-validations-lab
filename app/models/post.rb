c3b7b4d5b05f0285feab053190a4d54abbd63f2f
Griffin Dooley <dooleygriffin@gmail.com>
2022-07-21 11:39:56 -0400

    commit tree 68c9d9da46da23295b734e19d59ee4de95be0469

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :title_is_clickbait
+
+    def title_is_clickbait
+        if title == 'True Facts'
+            errors.add(:title, "We're only allowed to have titles that are clickbait!")
+        end
+    end
 end
